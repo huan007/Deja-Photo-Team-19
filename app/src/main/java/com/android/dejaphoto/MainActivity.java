7d1a373f1a239bb2775359b8dfce8e58076fc5a8
Andy Or <awor@ucsd.edu>
2017-05-03 20:08:10 -0700

    "Added SharedPreferences for Updating Interval"

--- app/src/main/java/com/android/dejaphoto/MainActivity.java
+++ app/src/main/java/com/android/dejaphoto/MainActivity.java
@@ -1,15 +1,20 @@
 package com.android.dejaphoto;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -35,13 +40,14 @@ public class MainActivity extends AppCompatActivity {
         switchDeja.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    // TO DO
-                    Toast.makeText( getApplicationContext(), "Deja Vu Mode ON", Toast.LENGTH_SHORT).show();
+                if (isChecked) {
+                    // ToDO Turn Deja Vu Mode On
+                    Toast.makeText(getApplicationContext(), "Deja Vu Mode ON", Toast.LENGTH_SHORT).show();
                 } else {
-                    // TO DO
-                    Toast.makeText( getApplicationContext(), "Deja Vu Mode OFF", Toast.LENGTH_SHORT).show();
+                    // ToDo Turn Deja Vu Mode Off
+                    Toast.makeText(getApplicationContext(), "Deja Vu Mode OFF", Toast.LENGTH_SHORT).show();
                 }
+                Log.d("Deja Vu Mode Preference", Boolean.valueOf(isChecked).toString());
             }
         });
 
@@ -49,11 +55,14 @@ public class MainActivity extends AppCompatActivity {
         switchLocation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    Toast.makeText( getApplicationContext(), "Location ON", Toast.LENGTH_SHORT).show();
+                if (isChecked) {
+                    // ToDO Turn Location Preference On
+                    Toast.makeText(getApplicationContext(), "Location ON", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText( getApplicationContext(), "Locatoin OFF", Toast.LENGTH_SHORT).show();
+                    // ToDO Turn Location Preference Off
+                    Toast.makeText(getApplicationContext(), "Locatoin OFF", Toast.LENGTH_SHORT).show();
                 }
+                Log.d("Location Preference", Boolean.valueOf(isChecked).toString());
             }
         });
 
@@ -61,11 +70,14 @@ public class MainActivity extends AppCompatActivity {
         switchDay.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                   Toast.makeText( getApplicationContext(), "Day of Week ON", Toast.LENGTH_SHORT).show();
+                if (isChecked) {
+                    // ToDO Turn Day of Week Preference On
+                    Toast.makeText(getApplicationContext(), "Day of Week ON", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText( getApplicationContext(), "Day of Week OFF", Toast.LENGTH_SHORT).show();
+                    // ToDO Turn Day of Week Preference Off
+                    Toast.makeText(getApplicationContext(), "Day of Week OFF", Toast.LENGTH_SHORT).show();
                 }
+                Log.d("Day of Week Preference", Boolean.valueOf(isChecked).toString());
             }
         });
 
@@ -73,15 +85,39 @@ public class MainActivity extends AppCompatActivity {
         switchTime.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    Toast.makeText( getApplicationContext(), "Time ON", Toast.LENGTH_SHORT).show();
+                if (isChecked) {
+                    // ToDO Turn Time Preference On
+                    Toast.makeText(getApplicationContext(), "Time ON", Toast.LENGTH_SHORT).show();
                 } else {
-                    Toast.makeText( getApplicationContext(), "Time OFF", Toast.LENGTH_SHORT).show();
+                    // ToDO Turn Time Preference Off
+                    Toast.makeText(getApplicationContext(), "Time OFF", Toast.LENGTH_SHORT).show();
                 }
+                Log.d("Time Preference", Boolean.valueOf(isChecked).toString());
             }
         });
 
+        final Button interval = (Button) findViewById(R.id.intervalButton);
+        interval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText inputInterval = (EditText) findViewById(R.id.inputInterval);
 
+                SharedPreferences sharedPreferences = getSharedPreferences("interval", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                editor.putString("newInterval", inputInterval.getText().toString());
+
+                editor.apply();
+                changeInterval();
+            }
+        });
+
+    }
+
+    public void changeInterval() {
+        SharedPreferences sharedPreferences = getSharedPreferences("newInterval", MODE_PRIVATE);
+        Toast.makeText(getApplicationContext(), "Changed Update Interval", Toast.LENGTH_SHORT).show();
+        //ToDO change update interval
     }
 
     @Override
