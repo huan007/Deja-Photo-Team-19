0f426248b6b772b1675851f1729f82485c320549
Andy Or <awor@ucsd.edu>
2017-05-03 19:37:34 -0700

    "Created Switches for Settings UI"

--- app/src/main/java/com/android/dejaphoto/MainActivity.java
+++ app/src/main/java/com/android/dejaphoto/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,58 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Switch switchDeja = (Switch) findViewById(R.id.DejaVu);
+        switchDeja.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    // TO DO
+                    Toast.makeText( getApplicationContext(), "Deja Vu Mode ON", Toast.LENGTH_SHORT).show();
+                } else {
+                    // TO DO
+                    Toast.makeText( getApplicationContext(), "Deja Vu Mode OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Switch switchLocation = (Switch) findViewById(R.id.Location);
+        switchLocation.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    Toast.makeText( getApplicationContext(), "Location ON", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText( getApplicationContext(), "Locatoin OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Switch switchDay = (Switch) findViewById(R.id.DayOfWeek);
+        switchDay.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                   Toast.makeText( getApplicationContext(), "Day of Week ON", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText( getApplicationContext(), "Day of Week OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        Switch switchTime = (Switch) findViewById(R.id.Time);
+        switchTime.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    Toast.makeText( getApplicationContext(), "Time ON", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText( getApplicationContext(), "Time OFF", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
     }
 
     @Override
