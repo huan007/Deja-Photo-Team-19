6b1eea26040ecc3beebd77d7a8cd7d88561bfb9f
cyraxnguyen <cyraxnguyen@gmail.com>
2017-05-04 19:00:13 -0700

    Finished widget with images

--- app/src/main/java/layout/AppWidget.java
+++ app/src/main/java/layout/AppWidget.java
@@ -3,6 +3,7 @@ package layout;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.view.View;
 import android.widget.RemoteViews;
 
 
@@ -16,7 +17,9 @@ import android.content.Intent;
 import android.util.Log;
 
 
+import com.android.dejaphoto.MainActivity;
 import com.android.dejaphoto.R;
+//import com.android.dejaphoto.SettingsActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -29,7 +32,6 @@ public class AppWidget extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -40,17 +42,34 @@ public class AppWidget extends AppWidgetProvider {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
+
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+            views.setOnClickPendingIntent(R.id.settingsButton, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
     }
 
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
+    private void openSettings()
+    {
+
+    }
+
+
+
 }
 
