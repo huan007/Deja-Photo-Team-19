e52d9162a63b3b92c633f1884e609b3dbdff0f27
Andy Or <awor@ucsd.edu>
2017-05-07 14:52:12 -0700

    Photo Object and Class

---
+++ app/src/main/java/com/android/dejaphoto/Photo.java
@@ -0,0 +1,16 @@
+package com.android.dejaphoto;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Phillip on 5/3/17.
+ */
+
+public class Photo {
+    String location;
+    String datetime;
+    String latitude;
+    String longitude;
+    Bitmap photo;
+
+}
