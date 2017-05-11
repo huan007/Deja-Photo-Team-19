e3725a9cc269768cd56640e93518ba04c1a71fa1
blt008 <blt008@ucsd.edu>
2017-05-10 17:10:39 -0700

    Fix Picasso import

--- app/src/main/java/com/android/dejaphoto/ImageController.java
+++ app/src/main/java/com/android/dejaphoto/ImageController.java
@@ -1,22 +1,15 @@
 package com.android.dejaphoto;
 
-import android.app.Activity;
-import android.app.Application;
 import android.app.WallpaperManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Point;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Display;
-import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.ImageView;
 
-import com.android.dejaphoto.MainActivity;
-import com.android.dejaphoto.R;
 import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
@@ -44,51 +37,46 @@ public class ImageController {
      * Change the wallpaper
      */
     public void displayImage(Photo photo) {
-        try {
+        Display display = ((WindowManager) currentContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int width = size.x;
+        int height = size.y;
 
-            Display display = ((WindowManager) currentContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
-            Point size = new Point();
-            display.getSize(size);
-            int width = size.x;
-            int height = size.y;
-
-            target = new Target() {
-                @Override
-                public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
-                    try {
-                        System.out.println(bitmap.getWidth() + " " + bitmap.getHeight());
-                        wallpaperManager.setBitmap(photo.appendLocation(currentContext, bitmap));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                try {
+                    Log.d("ImageController", "Wallpaper Changed");
+                    wallpaperManager.setBitmap(photo.appendLocation(currentContext, bitmap));
+                } catch (IOException e) {
+                    Log.d("ImageController", "Wallpaper Not Changed");
+                    e.printStackTrace();
                 }
+            }
 
-                @Override
-                public void onBitmapFailed(Drawable errorDrawable) {
-                }
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+                Log.d("ImageController", "Wallpaper Not Changed");
+            }
 
-                @Override
-                public void onPrepareLoad(Drawable placeHolderDrawable) {
-                }
-            };
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+            }
+        };
 
-            Runnable runnable = new Runnable() {
-                @Override
-                public void run() {
-                    Picasso.with(currentContext)
-                            .load(Photo.getImageUri(currentContext, photo.photo))
-                            .resize(width, height)
-                            .centerCrop()
-                            .into(target);
-                }
-            };
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Picasso.with(currentContext)
+                        .load(Photo.getImageUri(currentContext, photo.photo))
+                        .resize(width, height)
+                        .centerCrop()
+                        .into(target);
+            }
+        };
 
-            new Handler(currentContext.getMainLooper()).post(runnable);
-            Log.d("ImageController", "Wallpaper Changed");
-        } catch (Exception e) {
-            Log.d("ImageController", "Failed to change wallpaper");
-            e.printStackTrace();
-        }
+        new Handler(currentContext.getMainLooper()).post(runnable);
     }
 
     /**
