fc0dfd1740aaa7f36ae0679ea6476d82a561c52b
blt008 <blt008@ucsd.edu>
2017-05-11 15:16:57 -0700

    Change gradle things to hopefully speed up building and get rid of those errors

--- app/src/main/java/com/android/dejaphoto/MainActivity.java
+++ app/src/main/java/com/android/dejaphoto/MainActivity.java
@@ -4,13 +4,10 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -25,7 +22,6 @@ import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
 import com.google.android.gms.location.LocationServices;
 
 import java.io.File;
-import java.text.BreakIterator;
 
 import static com.google.android.gms.common.api.GoogleApiClient.*;
 
@@ -186,7 +182,7 @@ public class MainActivity extends AppCompatActivity
             super.onCreate(saveInstanceState);
             addPreferencesFromResource(R.xml.preferences);
 
-            SharedPreferences.Editor editor = getContext().getSharedPreferences("settings", MODE_PRIVATE).edit();
+            final SharedPreferences.Editor editor = getContext().getSharedPreferences("settings", MODE_PRIVATE).edit();
 
             // set button change listener for Interval Setting
             findPreference("interval").setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
