06ea176b5536119a6f2ba73cf9fd04397464ff4c
blt008 <blt008@ucsd.edu>
2017-05-12 14:36:44 -0700

    Fix out of memory error when loading large images

--- app/src/main/java/com/android/dejaphoto/ImageController.java
+++ app/src/main/java/com/android/dejaphoto/ImageController.java
@@ -2,11 +2,17 @@ package com.android.dejaphoto;
 
 import android.app.WallpaperManager;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.util.Log;
 import android.view.WindowManager;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
 
 import java.io.IOException;
 
@@ -29,30 +35,43 @@ public class ImageController {
      * Change the wallpaper to specified photo.
      */
     public void displayImage(Photo photo) {
-        Point size = new Point();
+        // get size of display
+        final Point size = new Point();
         ((WindowManager) currentContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getSize(size);
 
-        boolean default_background = (photo == null);
-
-        // create default photo if photo is null
-        if (default_background) {
-            Log.d("ImageController", "create default wallpaper");
-            photo = new Photo();
-            photo.photo = BitmapFactory.decodeResource(currentContext.getResources(), R.drawable.apple);
-        }
+        final boolean default_background = (photo == null);
+        final Photo background = (default_background ? new Photo() : photo);
 
         // set wallpaper
-        try {
-            Log.d("ImageController", "change wallpaper successful");
-            wallpaperManager.setBitmap(PhotoEditor.start(photo)
-                    .fitScreen(size.x, size.y, !default_background)
-                    .appendLocation(currentContext)
-                    .addText(default_background ? "No Photos in Gallery" : "")
-                    .finish());
-        } catch (IOException e) {
-            Log.d("ImageController", "change wallpaper unsuccessful");
-            e.printStackTrace();
-        }
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Log.d("ImageController", "changing wallpaper");
+                Glide.with(currentContext)
+                        .load(default_background ? R.drawable.apple : background.getImageUri(currentContext))   // load background
+                        .asBitmap()                                                                             // as bitmap
+                        .into(new SimpleTarget<Bitmap>() {
+                            @Override
+                            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                                try {
+                                    wallpaperManager.setBitmap(PhotoEditor.start(background, resource)          // modify bitmap
+                                            .fitScreen(size.x, size.y, !default_background)                     // fit to screen
+                                            .appendLocation(currentContext)                                     // add location
+                                            .addText((default_background) ? "No Photos in Gallery" : "")        // add error
+                                            .finish());                                                         // set wallpaper
+                                    Log.d("ImageController", "change wallpaper successful");
+                                } catch (IOException e) {
+                                    Log.d("ImageController", "change wallpaper unsuccessful");
+                                    e.printStackTrace();
+                                }
+                            }
+                        });
+            }
+        };
+
+        // set wallpaper using the main thread
+        new Handler(currentContext.getMainLooper()).post(runnable);
+
     }
 
     /**
