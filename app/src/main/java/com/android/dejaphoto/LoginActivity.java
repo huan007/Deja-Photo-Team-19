fb08948853a1717c04d15cfd065f02c912bff7f3
blt008 <blt008@ucsd.edu>
2017-06-01 15:06:12 -0700

    Open main activity after login and store email

--- app/src/main/java/com/android/dejaphoto/LoginActivity.java
+++ app/src/main/java/com/android/dejaphoto/LoginActivity.java
@@ -1,16 +1,14 @@
 package com.android.dejaphoto;
 
 import android.content.Intent;
-import android.os.Build;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -27,8 +25,6 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
-import com.google.firebase.database.FirebaseDatabase;
-
 
 public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
@@ -104,10 +100,15 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
             statusTextView.setText("Hello, " + acct.getDisplayName());
 
             //Change Activity
-            Intent intent = new Intent (this, InteractiveActivity.class);
+            Intent intent = new Intent (this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            intent.putExtra("UID", acct);
             startActivity(intent);
+
+            // store email inside shared preference
+            SharedPreferences.Editor editor = getSharedPreferences("user", MODE_PRIVATE).edit();
+            editor.putString("email", FirebaseManager.createID(acct.getEmail()));
+            editor.apply();
+
             finish();
         }else{
 
