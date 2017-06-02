7c2e78ef9eb47a6713ec711b63e856c762a0fb67
cyraxnguyen <cyraxnguyen@gmail.com>
2017-06-02 16:18:16 -0700

    Added dummy Implementation of Adapters

---
+++ app/src/main/java/com/android/dejaphoto/FirebaseDatabaseAdapter.java
@@ -0,0 +1,146 @@
+package com.android.dejaphoto;
+
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.List;
+
+/**
+ * Created by Cyrax on 6/2/2017.
+ */
+
+class FirebaseDatabaseAdapter implements FirebaseDatabaseAdapterInterface {
+    private static final FirebaseDatabaseAdapter ourInstance = new FirebaseDatabaseAdapter();
+
+    static FirebaseDatabaseAdapter getInstance() {
+        return ourInstance;
+    }
+
+    private FirebaseDatabaseAdapter() {
+    }
+
+    /**
+     * Used to get user's info stored in database every time user log in
+     *
+     * @param email E-mail address of the user requested
+     * @return User object that is stored on the Database. Will return NULL if the user requested
+     * does not exist
+     */
+    @Override
+    public User getUserFromDatabase(String email) {
+        return null;
+    }
+
+    /**
+     * Create a new user entry in the database
+     *
+     * @param email   E-mail address of the new user
+     * @param newUser User object that need to be injected into database. Contains list of friends
+     *                and list of photos
+     * @return
+     */
+    @Override
+    public User createNewUser(String email, User newUser) {
+        return null;
+    }
+
+    /**
+     * Return a list of photos shared by the specified user
+     *
+     * @param email E-mail address of the specified user
+     * @return List of photos shared by the user
+     */
+    @Override
+    public List<Photo> getListOfPhotoFromUser(String email) {
+        return null;
+    }
+
+    /**
+     * Extract information from Photo object and create a new entry in current user's list of photo
+     *
+     * @param newPhoto new photo that will be added to the databse
+     * @return returns TRUE if the photo was successfully added. FALSE if not.
+     */
+    @Override
+    public boolean addNewPhotoEntry(Photo newPhoto) {
+        return false;
+    }
+
+    /**
+     * Check whether or not the photo is already in the database
+     *
+     * @param photo photo to be checked
+     * @return returns TRUE if the photo is already in the database. FALSE if not.
+     */
+    @Override
+    public boolean checkPhotoEntry(Photo photo) {
+        return false;
+    }
+
+    /**
+     * Remove photo entry from Firebase Database
+     *
+     * @param photo photo to be removed
+     * @return returns TRUE if photo is successfully removed. FALSE if the photo is not there.
+     */
+    @Override
+    public boolean removePhotoEntry(Photo photo) {
+        return false;
+    }
+
+    /**
+     * Get handle to requested user's photo list. Used to create Listeners
+     *
+     * @param email E-mail address of specified user
+     * @return returns a reference to specified user's photo list. Returns NULL if user doesn't
+     * exist
+     */
+    @Override
+    public DatabaseReference getUserPhotoReference(String email) {
+        return null;
+    }
+
+    /**
+     * Add new friend using email
+     *
+     * @param email E-mail address of the new friend. Used to identify the friend
+     * @return returns TRUE if new friend entry is created. FALSE if not.
+     */
+    @Override
+    public boolean addNewFriendEntryByEmail(String email) {
+        return false;
+    }
+
+    /**
+     * Check whether the friend exist in the current user's friend list
+     *
+     * @param email E-mail address of the friend. Used to identify the friend
+     * @return returns TRUE if friend is in the user's friend list. FALSE if not.
+     */
+    @Override
+    public boolean checkFriendEntryByEmail(String email) {
+        return false;
+    }
+
+    /**
+     * Remove friend from user's friend list
+     *
+     * @param email E-mail address of the specified friend. Used to identify the friend
+     * @return returns TRUE if friend is successfully removed. FALSE if the friend is not there.
+     */
+    @Override
+    public boolean removeFreindEntryByEmail(String email) {
+        return false;
+    }
+
+    /**
+     * Get handle to specified user's friend list. Used to create Listeners
+     *
+     * @param email E-mail address of the specified user
+     * @return returns a reference to specified user's friend list. Returns NULL if the user doesn't
+     * exist
+     */
+    @Override
+    public boolean getUserFriendReference(String email) {
+        return false;
+    }
+}
---
+++ app/src/main/java/com/android/dejaphoto/FirebaseStorageAdapter.java
@@ -0,0 +1,135 @@
+package com.android.dejaphoto;
+
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
+/**
+ * Created by Cyrax on 6/2/2017.
+ */
+
+class FirebaseStorageAdapter implements FirebaseStorageAdapterInterface {
+    private static FirebaseStorageAdapter ourInstance = null;
+
+    //Used to take care of Storage references
+    private static String currUserEmail;
+    private static StorageReference currUserDir;
+    private static StorageReference rootDir;
+
+    //Tag used for debug
+    private String debug_tag = "AdapterStorage";
+
+    static FirebaseStorageAdapter getInstance() {
+
+        //Using lazy instantiation
+        if (ourInstance == null)
+        {
+            ourInstance = new FirebaseStorageAdapter();
+
+            //Used to take care of Storage references
+            currUserEmail = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+            currUserDir = FirebaseStorage.getInstance().getReference().child(currUserEmail);
+            rootDir = FirebaseStorage.getInstance().getReference();
+        }
+        return ourInstance;
+    }
+
+    private FirebaseStorageAdapter() {
+    }
+
+    /**
+     * Upload a photo file to the database
+     *
+     * @param photoFile file representing the photo
+     * @return returns an UploadTask, which can then be used to manage the upload of the photo
+     */
+    @Override
+    public UploadTask uploadPhotoFile(File photoFile) {
+        //Start an upload task
+        Uri fileUri = Uri.fromFile(photoFile);
+        StorageReference targetReference = currUserDir.child(fileUri.getLastPathSegment());
+        UploadTask task = targetReference.putFile(fileUri);
+
+        //Added for debugging purposes
+        task.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                int errorCode = ((StorageException) exception).getErrorCode();
+                String errorMessage = exception.getMessage();
+                Log.d(debug_tag, "Error Message: " + errorMessage);
+            }
+        });
+        return task;
+    }
+
+    /**
+     * Download a photo from the database from a user's space
+     *
+     * @param email         E-mail address of the user. Used to locate the user's directory
+     * @param photoFileName name of the requested photo file
+     * @return returns TRUE if photo exists and downloaded. FALSE if not.
+     */
+    @Override
+    public boolean downloadPhotoFromUser(String email, String photoFileName) {
+        return false;
+    }
+
+    /**
+     * Remove the specified photo files from current user's storage
+     *
+     * @param photoFileName name of the photo file to be removed
+     * @return returns TRUE if photo exists and removed. FALSE if not.
+     */
+    @Override
+    public boolean removePhoto(String photoFileName) {
+        return false;
+    }
+
+    /**
+     * Check whether or not the photo exist in the current user's storage
+     *
+     * @param photoFileName name of the photo file requested
+     * @return returns TRUE if photo exists. FALSE if not.
+     */
+    @Override
+    public boolean checkPhotoExistInCurrentUser(String photoFileName) {
+        StorageReference fileReference = currUserDir.child(photoFileName);
+        final boolean[] isExist = {false};
+
+        fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                isExist[0] = true;
+                Log.d(debug_tag, uri.toString());
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                isExist[0] = false;
+            }
+        });
+        return isExist[0];
+    }
+
+    /**
+     * Check whether or not the photo exist in the specified user's storage
+     *
+     * @param photoFileName name of the photo file requested
+     * @return returns TRUE if photo exists. FALSE if not.
+     */
+    @Override
+    public boolean checkPhotoExistInSpecifiedUser(String photoFileName) {
+        return false;
+    }
+}
